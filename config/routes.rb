bb7af72b80b51cd463ec76809ca9ef5d5da4b886
Josh Frankel <joshmfrankel@gmail.com>
2023-04-27 15:32:21 -0400

    Add github action and render.com health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -5,4 +7,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   post "/slack/commands", to: "slack/commands#create"
   post "/slack/interactions", to: "slack/interactions#create"
+
+  # Render.com uptime check
+  get "/render/health_checks", to: "render/health_checks#index"
 end
